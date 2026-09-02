186beea90da9ce46e4a13790896c02d8041296ef
Rafael Hiroshi Tejima <129396110+RafaelTejima@users.noreply.github.com>
2026-09-02 17:11:14 -0300

    Aula 01 de React realizada

--- introducao/src/App.jsx
+++ introducao/src/App.jsx
@@ -1,10 +1,29 @@
-
 function App() {
+
+  let cliques = 0 // useState
+
+  let nome = "Rafael"
+  let cidade = "São Carlos"
+  let nome_completo = " Hiroshi Tejima"
+
+
+  function contarCliques(){
+    cliques += 1
+    
+  }
+
 
   return (
    <div>
-    <h1>Olá Mundo!</h1>
-    <p>Estou aprendendo React xD</p>
+    <h1>Olá {2+2} Mundo!</h1>
+    <p>Estou apren{console.log("flores")}dendo React xD</p>
+    <hr/>
+    <p>Meu nome é {nome} {nome_completo} sou da cidade de {cidade}.</p>
+        
+    <hr/>
+
+    <p>Você clicou <strong>{cliques}</strong> vezes</p>
+    <button onClick={contarCliques} >Clique aqui</button>
    </div>
   )
 }
