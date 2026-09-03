2688881dbca8b557d47095b2e90c11f4a1ec9aca
Rafael Hiroshi Tejima <129396110+RafaelTejima@users.noreply.github.com>
2026-09-03 17:16:41 -0300

    Formularios feitos

---
+++ introducao/src/Formularios.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react"
+
+function Formularios(){
+
+    let [nome, setNome] = useState()
+    let [idade, setIdade] = useState()
+    let [cidade, setCidade] = useState()
+    let [estado, setEstado] = useState()
+
+    function salvar(){
+
+        if(nome == ""){
+            alert("Nome inválido.")
+            return
+        }
+
+        if(idade <= 0){
+            alert("Idade inválida")
+            return
+        }
+
+        if(cidade == null){
+            alert("Cidade inválida")
+            return
+        }
+
+        if(estado == null){
+            alert("Estado inválido")
+            return
+        }
+
+        alert("seja bem vindo(a)! " +nome+ "\nVocê tem " +idade+ " anos" + "\nMora em " +cidade+ "\nDo estado de " +estado)        
+    }
+
+    return(
+        <div>
+
+            <h1>Página de formulário</h1>
+            <p>Aprendendo a usar o input no React</p>
+
+            <p>Digite seu nome: </p>
+            <input onChange={ e => setNome(e.target.value) }/>
+            <br/>
+
+            <p>Digite sua idade: </p>
+            <input onChange={e => setIdade(e.target.value)}/>
+
+            <br/>
+            <p>Digite onde mora </p>
+            <input placeholder="Cidade" onChange={e => setCidade(e.target.value)} />
+            <input placeholder="Estado" onChange={e => setEstado(e.target.value)} />
+            <br/>
+            <br/>
+            <button onClick={salvar}>Salvar</button>
+
+
+
+        </div>
+    )
+}
+
+export default Formularios
