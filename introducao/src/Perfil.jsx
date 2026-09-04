9c3c6cbcb26d598e8e238811e90fd128e6d59857
Rafael Hiroshi Tejima <129396110+RafaelTejima@users.noreply.github.com>
2026-09-04 20:26:56 -0300

    atividade feita

--- introducao/src/Perfil.jsx
+++ introducao/src/Perfil.jsx
@@ -1,15 +1,27 @@
-import { useState } from "react"
+import { useState, useSyncExternalStore } from "react"
 
 function Perfil(){
 
     let [handleProfile, handleProfileUpdate] = useState(false)
     let [mostrarAlterarSenha, mostrandoAlterarSenha] = useState(false)
-    let [alterarSenha, senhaAlterada] = useState()
-
+    let [senhaAtual, setSenhaAtual] = useState("")
+    let [novaSenha, setNovaSenha] = useState("")
+    
     let usuario = {
         nome: "Abel",
         email: "genehackerman@gmail.com",
         senha: "123123"
+    }
+
+    function confirmar(){
+        
+        if(senhaAtual == usuario.senha){
+            alert("sua senha foi alterada")
+            mostrandoAlterarSenha(false)
+        } else{
+            alert("sua senha está incorreta")
+        }
+
     }
 
     return(
@@ -26,7 +38,7 @@ function Perfil(){
             <div>
                 <p>Nome: {usuario.nome}</p>
                 <p>Email: {usuario.email}</p>
-                <button>Alterar senha</button>
+                <button onClick={ ()=> mostrandoAlterarSenha(!mostrarAlterarSenha)} >Alterar senha</button>
             </div>
                      
                 : 
@@ -35,16 +47,28 @@ function Perfil(){
 
             }
 
-                <div>
-                    <h1>Alterar senha</h1>
-                    <p>Digite sua senha atual</p>
-                    <input/>
+            {
+                
+                
+                mostrarAlterarSenha == true ? 
+                            
+            <div>
+                <h1>Alterar senha</h1>
+                <p>Digite sua senha atual</p>
+               <input onChange={ e => setSenhaAtual(e.target.value)}/>
+                
+                <p>Digite sua nova senha</p>
+                <input/>
+                <br/><br/>
+                <button onClick={ ()=> confirmar()}>Confirmar</button>
+            </div>
 
-                    <p>Digite sua nova senha</p>
-                    <input/>
-                    <br/>
-                    <button>Confirmar</button>
-                </div>
+             :
+                
+           <p></p>
+
+            }
+
 
         </div>
     )
