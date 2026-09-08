7bf061385d3f7c592199d5f52ad4ca64601397e3
Rafael Hiroshi Tejima <129396110+RafaelTejima@users.noreply.github.com>
2026-09-08 17:05:21 -0300

    aula sobre consumo de API adicionada

---
+++ consumoAPI/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+function App() {
+
+    const [usuarios, alterarUsuarios] = useState([])
+    const [mostrarLista, mostrandolista] = useState(false)
+
+    async function buscarTodos(){
+
+        const response = await fetch("https://dummyjson.com/users")
+        const data = await response.json()
+        console.log(data)
+        alterarUsuarios(data.users)
+
+    }
+
+    function mostrarInformações(usuario){
+        alert("telefone: " + usuario.phone + "\nemail: " + usuario.email + "\nmora em: " + usuario.address.city + " - " + usuario.address.state)
+    }
+
+    return (
+
+        <div>
+
+            <h1>Consumo da API</h1>
+            <p>Buscando dados da API DummyJSON</p>
+            <button onClick={buscarTodos} >Carregar dados</button>
+
+            <ul>
+                {
+                    usuarios.length == 0 ? 
+                        <p>lista vazia...</p>
+                    :
+                        usuarios.map(
+                            i =>  <li> Sr(a) {i.firstName} tem {i.age} anos <button onClick={ ()=> mostrarInformações(i)}>Ver informações</button> </li>
+                        )
+                } 
+            </ul>
+
+        </div>
+
+    );
+}
+
+export default App
